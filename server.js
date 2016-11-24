abd0ddd18af11e3588c3c72bfb4aa438c69cf4ee
ashwin <ashwinm73@gmail.com>
2016-11-24 15:36:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=required('pg').pool;
+var confi={
+    user:'ashwinm73',
+    database:'ashwinm73',
+    host:'db,imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB-PASSWORD
+    
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -11,7 +21,18 @@ app.get('/', function (req, res) {
 app.get('/click', function(req,res){
     res.sendfile(path.join(__dirname, 'ui', "click.html"));
 });
+var pool = new Pool(config);
 
+app.get('/test-db',function(req,res){
+    pool.query('select*fromtest',function(err,result){
+        if(err){
+            res.status(500).send(err.tostring());
+        }else{
+            res.send(json.stringify(result));
+        }
+    });
+    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
