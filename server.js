cd20a4bdc9a413023c8d25568e57b732153abb6c
ashwin <ashwinm73@gmail.com>
2016-11-25 14:34:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
  var Pool = require('pg').Pool;
- var confi={
+ var config={
  user:'ashwinm73',
  database:'ashwinm73',
  host:'db,imad.hasura-app.io',
