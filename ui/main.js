46d76baebf9cf4141489f04b883d77da5b901e2e
ashwin <ashwinm73@gmail.com>
2016-11-25 09:55:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+var nameinput=document.getelementbyId('name');
+var name=nameinput.value;
+var submit=document.getelementbyId('submit');
+submit.onclick=function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;names.length;i++){
+        list +='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getelementbyId('namelist');
+    ul.innerHTML=list;
+    
+};
