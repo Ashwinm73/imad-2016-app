d1e3db10314ea9088a451179a6e6b2984692ccfd
ashwin <ashwinm73@gmail.com>
2016-11-25 10:20:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,27 @@ var nameinput=document.getelementbyId('name');
 var name=nameinput.value;
 var submit=document.getelementbyId('submit');
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
+    var request = new xmlhttprequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readystate=== xmlhttprequest.DONE) {
+            if(request.readystate===200) {
+                
+    
+    var names = request.responseText;
+    names=JSON.parse(names);
     var list='';
     for(var i=0;names.length;i++){
         list +='<li>'+names[i]+'</li>';
     }
     var ul=document.getelementbyId('namelist');
     ul.innerHTML=list;
+  }
+  
+  }
     
+};
+
+request.open('GET', 'http://http://ashwinm73.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
 };
