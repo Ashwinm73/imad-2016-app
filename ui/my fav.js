c9d829aebf26d1b577062e91e8cc98f7d732faec
ashwin <ashwinm73@gmail.com>
2016-11-26 16:49:29 +0530

    [imad-console] Updates ui/my fav.js

--- ui/my fav.js
+++ ui/my fav.js
@@ -10,18 +10,15 @@ function loadCommentForm () {
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
-    // Submit username/password to login
     var submit = document.getElementById('submit');
     submit.onclick = function () {
-        // Create a request object
         var request = new XMLHttpRequest();
         
-        // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
                 if (request.status === 200) {
-                    // clear the form & reload all the comments
+                
                     document.getElementById('comment_text').value = '';
                     loadComments();    
                 } else {
@@ -31,7 +28,6 @@ function loadCommentForm () {
           }
         };
         
-        // Make the request
         var comment = document.getElementById('comment_text').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
@@ -42,7 +38,6 @@ function loadCommentForm () {
 }
 
 function loadLogin () {
-    // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -65,7 +60,7 @@ function escapeHTML (text)
 }
 
 function loadComments () {
-        // Check if the user is already logged in
+
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -94,6 +89,6 @@ function loadComments () {
 }
 
 
-// The first thing to do is to check if the user is logged in!
+
 loadLogin();
 loadComments();
