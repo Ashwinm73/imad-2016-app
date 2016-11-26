174b87ca093936e394fae74e69801f20664ed701
ashwin <ashwinm73@gmail.com>
2016-11-26 18:47:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,6 @@ function loadLoginForm () {
           if (request.readyState === XMLHttpRequest.DONE) {
              
               if (request.status === 200) {
-                   alert('Sucessfully saved in our database');
                   submit.value = 'Sucess!';
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
